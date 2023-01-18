ca9dc4418a4d2fad1b3b6538f1355966787dc573
Steve <48702437+erwin-huang@users.noreply.github.com>
2023-01-18 15:18:30 +0800

    Commit from TeleportHQ

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { AppStateProvider } from './store' 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './teleporthq/style.css'
+import Home from './teleporthq/pages/home'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const App = () => {
+  return (
+    <Router>
+      <AppStateProvider>
+        <Route component={Home} exact path="/" />
+      </AppStateProvider>
+    </Router>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('app'))
